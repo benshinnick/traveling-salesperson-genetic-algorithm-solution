bfcd60de662ad727337845bcbfa1f32470a2dd7b
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-03-17 12:04:15 -0500

    defined brute force functions

--- src/brute-force-algorithm.hpp
+++ src/brute-force-algorithm.hpp
@@ -1,12 +1,26 @@
 #ifndef BRUTE_FORCE_ALGORITHM_HPP
 #define BRUTE_FORCE_ALGORITHM_HPP
 
+#include "city-graph.hpp"
+#include "tour.hpp"
+
 class BruteForceAlgorithm {
 
+    static const int MAX_PERMUTATION_SIZE = 19;
+
     private:
+        int numOfCities;
+        int currPermuation[MAX_PERMUTATION_SIZE];
+        double lowestTourCost;
+        Tour currTour;
+
+        void getNextPermuation();
+        void getNextTour();
 
     public:
-    //    void getNextPermuation(int prevPermuation[], int numOfCities);  
+        BruteForceAlgorithm(int numOfCities);
+        void runBruteFoceAlgorithm();
+        double getLowestFoundTourCost();
 };
 
 #endif
