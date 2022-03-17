7ac1ce62eedcda4e5e3b3df8f38e3f6cf5b55e03
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-03-17 11:24:51 -0500

    defined tour functions

--- src/tour.hpp
+++ src/tour.hpp
@@ -3,9 +3,18 @@
 
 class Tour {
 
+    static const int MAX_TOUR_SIZE = 21;
+
     private:
+        int tourArray[MAX_TOUR_SIZE];
+        int numCitiesInTour;
+        double calculateTourCost();
 
     public:
+        Tour(int permutation[], int numCitiesInTour);
+        double getTourCost();
+        void setTourCity(int cityIndex, int city);
+        int getTourCity(int cityIndex);
         
 };
 
