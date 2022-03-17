1fc16c11b80b027c57864bb83c354f65e8692975
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-03-17 11:09:06 -0500

    defined CityGraph functions

--- src/city-graph.cpp
+++ src/city-graph.cpp
@@ -1,1 +1,17 @@
 #include "city-graph.hpp"
+
+void CityGraph::fillCityDistancesFromInputFile() {
+    std::ifstream distancesFileInput;
+}
+
+CityGraph::CityGraph() {
+
+}
+
+int CityGraph::setDistance(int departureCity, int arrivalCity, int distance) {
+
+}
+
+int CityGraph::getDistance(int departureCity, int arrivalCity) {
+
+}
--- src/city-graph.hpp
+++ src/city-graph.hpp
@@ -1,12 +1,22 @@
 #ifndef CITY_GRAPH_HPP
 #define CITY_GRAPH_HPP
 
+#include <fstream>
+#include <string>
+
 class CityGraph {
 
-    private:
+    static const int MAX_NUM_CITIES = 20;
+    const std::string DISTANCES_INPUT_FILE_NAME = "distances.txt";
 
-    public:
+    private:
+        int cityDistances[MAX_NUM_CITIES][MAX_NUM_CITIES];
+        void fillCityDistancesFromInputFile();
         
+    public:
+        CityGraph();
+        int setDistance(int departureCity, int arrivalCity, int distance);
+        int getDistance(int departureCity, int arrivalCity);
 };
 
 #endif
