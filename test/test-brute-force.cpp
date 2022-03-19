9be0c61189f4d7e9ca0f7c4099f3b532f28514f5
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-03-19 16:21:54 -0500

    Updated brute force test cases

--- test/test-brute-force.cpp
+++ test/test-brute-force.cpp
@@ -1,12 +1,14 @@
 #include "catch/catch.hpp"
 #include "../src/brute-force-algorithm.hpp"
 
-TEST_CASE("Test basic functionality") {
+TEST_CASE("Test Brute Force NumCities = 7") {
     BruteForceAlgorithm sut = BruteForceAlgorithm(7);
     sut.runBruteFoceAlgorithm();
 	REQUIRE(sut.getLowestFoundTourCost() > 0);
+}
 
-    sut = BruteForceAlgorithm(5);
+TEST_CASE("Test Brute Force numCities = 5") {
+    BruteForceAlgorithm sut = BruteForceAlgorithm(5);
     sut.runBruteFoceAlgorithm();
 	REQUIRE(sut.getLowestFoundTourCost() > 0);
 }
